23575ceaa00491ae90880ec67d23072ee61e3bf1
Victor-OT <crowbreaker96@hotmail.com>
2024-06-26 19:56:53 -0600

    en el Home se hizo una petición a una API de productos, se utilizó useState para guardar la información, se mandó esa información como prop de Card y ahí se utilizó la información recibida para llenar los datos de las card con la de la API, posteriormente se hizo un map de cada elemento de la api en el home para la creación de cada una de las cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 
 function Home () {
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+    }, [])
+
     return (
         <Layout>
             Home
-            <Card />
+            <div className='grid grid-cols-4 gap-20 w-full max-w-screen-lg'>
+                {
+                    items?.map(item => (
+                        <Card 
+                        data={item}
+                        key={item.id}
+                        />
+                    ))
+                }   
+            </div>
         </Layout>
     )
 }
