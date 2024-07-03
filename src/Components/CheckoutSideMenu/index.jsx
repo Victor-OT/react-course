362de70f0dfd2814e7fd4d72ec196d724f0477bf
Victor-OT <crowbreaker96@hotmail.com>
2024-07-03 15:30:49 -0600

    Se modificó el link de las ordenes, ahora la key toma como valor el index, y el enlace al final va a recibir igualmente el index

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -18,6 +18,7 @@ function CheckoutSideMenu () {
         }
         context.setOrder([...context.order, orderToAdd])
         context.setCartProducts([])
+        context.setCount(0)
     }
     
     return (
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,5 +1,6 @@
-import { Layout } from "../../Components/Layout"
 import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { Layout } from "../../Components/Layout"
 import { ShoppingCartContext } from "../../Context"
 import { totalPrice } from "../../Utils"
 import { OrdersCard } from "../../Components/OrdersCard"
@@ -11,10 +12,12 @@ function MyOrders () {
             <h1>My Orders</h1>
             <div>
                 {
-                    context.order.map(order => (
-                        <OrdersCard 
-                        totalProducts={order.products.length}
-                        totalPrice={totalPrice(order.products)}/>
+                    context.order.map((order, index) => (
+                        <Link key={index} to={`/my-order/${index}`}>
+                            <OrdersCard 
+                            totalProducts={order.products.length}
+                            totalPrice={totalPrice(order.products)} />
+                        </Link>
                     ))
                 }
             </div>
