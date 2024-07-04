0f2bfe7af29a7d509bf28fe9b34b8c191baef7d8
Victor-OT <crowbreaker96@hotmail.com>
2024-07-03 20:07:55 -0600

    Se estilizó OrdersCard

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,12 +1,17 @@
+import { ChevronRightIcon } from "@heroicons/react/16/solid"
+
 function OrdersCard (props) {
     const {totalProducts, totalPrice} = props
     return (
-        <div>
-            <p>
+        <div className='flex justify-between items-center gap-20 border border-black p-2 m-4 rounded-lg '>
+            <div className='flex flex-col items-center'>
                 <span>01.02.24</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice} </span>
-            </p>
+                <span>Articles: {totalProducts}</span>
+            </div>
+            <div className='flex items-center gap-2'>
+                <span className='text-xl font-semibold'>${totalPrice} </span>
+                <ChevronRightIcon className='size-6'/>
+            </div>
         </div>
     )
 }
