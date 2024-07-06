088ff87648029afd43f4f6f72537a95722dc8339
Victor-OT <crowbreaker96@hotmail.com>
2024-07-05 19:36:48 -0600

    Se creó un estado view que almacena un string indicador de qué vista renderizar en sign in. Se creó una variable para almacenar account del localStorage, y otra con esa información parseada, adicional se crearon dos variables más si devuelven true or false dependiendo de si existe información en el account del localStorage y del contexto, y una última que devuelve true or false si las dos condiciones anteriores se cumplen. Se crearon dos funciones: renderLogin y renderCreateUserInfo, que contienen la información de las vistas respectivamente (renderCreateUserInfo aún no contiene nada). En el botón Login y Sign up se hizo la lógica de desactivar el botón dependiendo de si hay una cuenta o no, y en Sign Up al hacer click cambia el valor del estado view a create-user-info

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,29 +1,64 @@
+import { useContext, useState } from "react"
+import { ShoppingCartContext } from "../../Context"
 import { Layout } from "../../Components/Layout"
 
 function SignIn () {
+    const context = useContext(ShoppingCartContext)
+    const [view, setView] = useState('user-info')
+    
+    //Account
+    const account = localStorage.getItem('account')
+    const parsedAccount = JSON.parse(account)
+
+    //Has an Account
+    const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+    const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+    const hasUserAnAccount = !noAccountInLocalStorage || ! noAccountInLocalState
+
+    const renderLogin = () => {
+        return (
+            <>
+                <div className='mb-4'>
+                    <p className='flex justify-start items-center gap-2'>
+                        <span className='text-md'>Email: </span>
+                        <span className='font-semibold text-lg'>{parsedAccount?.email}</span>
+                    </p>
+                    <p>
+                        <span className='text-md'>Password: </span>
+                        <span className='font-bold text-lg'>{parsedAccount?.password}</span>
+                    </p>
+                </div>
+                <div className='flex flex-col items-center gap-4'>
+                    <button 
+                        className='bg-black disabled:bg-black/40 w-full h-10 rounded-lg text-white'
+                        disabled={!hasUserAnAccount}>
+                        Log in
+                    </button>
+                    <span className='cursor-pointer underline underline-offset-4'>Forgot my password</span>
+                    <button 
+                        className='border border-black disabled:border-black/40 disabled:text-black/40 w-full h-10 rounded-lg font-semibold'
+                        disabled={hasUserAnAccount}
+                        onClick={setView('create-user-info')}>
+                        Sign up
+                    </button>
+                </div>
+            </>
+        )
+    }
+
+    const renderCreateUserInfo = () => {
+        return (
+            <div>Create Account</div>
+        )
+    }
+
+    const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogin()
+
     return (
         <Layout>
             <div className='flex flex-col'>
                 <h1 className='flex flex-col items-center font-semibold text-2xl m-8'>Welcome</h1>
-                <div className='mb-4'>
-                    <p className='flex justify-center items-center gap-2'>
-                        <span className='text-md'>Email: </span>
-                        <span className='font-semibold text-lg'>crowbreaker96@hotmail.com</span>
-                    </p>
-                    <p>
-                        <span className='text-md'>Password: </span>
-                        <span className='font-bold text-lg'>*******</span>
-                    </p>
-                </div>
-                <div className='flex flex-col items-center gap-4'>
-                    <button className='bg-black w-full h-10 rounded-lg text-white'>
-                        Log in
-                    </button>
-                    <span className='cursor-pointer underline underline-offset-4'>Forgot my password</span>
-                    <button className='border border-black w-full h-10 rounded-lg font-semibold'>
-                        Sign up
-                    </button>
-                </div>
+                {renderView()}
             </div>
         </Layout>
     )
